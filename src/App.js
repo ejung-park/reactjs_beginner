1c5d064a23fc474fc3952de3002d298183d19988
ejung <leejungstyle@gmail.com>
2019-12-04 22:50:13 +0900

    function args 보내는 방법

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import Potato from './Potato';
+import React from "react";
+
+function Food({favourite}) {
+  return <h1>I like {favourite}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food favourite="kimchi" />
     </div>
   );
 }
